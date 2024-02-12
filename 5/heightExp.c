6c586579e179ba2df07622f33cb87f8343155bf2
John Nguyen <nguyenhoangtu.427@gmail.com>
2024-02-12 14:31:18 -0500

    added the height experiments functions, generate the readme file for the data

--- 5/heightExp.c
+++ 5/heightExp.c
@@ -1,6 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "heightExp.h"
 #include "bst.h"
 
 void heightExperiments() {
-    return;
+    printf("Experiments (N=Number Element, Table Shows Height)\n");
+    printf("|   N  |   T1  |   T2  |   T3  |   T4  |   T5  |  Average |\n");
+    printf("| ---- | ----- | ----- | ----- | ----- | ----- | -------- |\n");
+
+    float sum;
+
+    for (int i = 2; i <= 1024; i = i*2) {
+        sum = 0;
+        printf("| %5d", i);
+        for (int j = 0; j < 5; j++) {
+            BST* bst = newBST();
+            for (int k = 0; k < i; k++) {
+                int target = rand();
+                insert(bst, target);
+            }
+            sum += height(bst);
+            printf("| %6d", height(bst));
+            deleteBST(bst);
+        }
+        printf("| %9.2f|\n", sum/5);
+    }
 }
